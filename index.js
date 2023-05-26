b5beada416f1a01ef6c5962c752be3427b0591b7
kingradas <pokornyr.04@spst.eu>
2023-05-26 09:20:52 +0200

    table generate, score board, goal count

--- index.js
+++ index.js
@@ -1,42 +1,77 @@
+// Získání odkazu na div s ID "goal"
+var goalDiv = document.querySelector('#goal-id');
+
+// Vytvoření prvku <table>
+var table = document.createElement('table');
+table.className = 'net';
+
+// Generování řádků a buněk
+for (let i = 0; i < 15; i++) {
+  var row = document.createElement('tr');
+  row.className = '';
+
+  for (let j = 0; j < 43; j++) {
+    var cell = document.createElement('td');
+    cell.className = '';
+    row.appendChild(cell);
+  }
+
+  table.appendChild(row);
+}
+
+// Přidání tabulky do divu
+goalDiv.appendChild(table);
+
+
 var ball = document.getElementById("ball");
 var canMove = true;
 
 function moveBallUp() {
     if (!canMove) return; // Pokud je pohyb zakázán, nedělej nic
     canMove = false; // Zakážeme pohyb na 5 sekund
-    ball.style.transition = "transform 1s, width 1s, height 1s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
+    ball.style.transition = "transform 0.5s, width 0.5s, height 0.5s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
     ball.style.transform = "translateY(-500%) scale(0.5)"; // Posun na ose y a zmenšení velikosti míče
+    scoreGoal()
 }
 
 
 function moveBallRight() {
     if (!canMove) return; // Pokud je pohyb zakázán, nedělej nic
     canMove = false; // Zakážeme pohyb na 5 sekund
-    ball.style.transition = "transform 1s, width 1s, height 1s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
+    ball.style.transition = "transform 0.5s, width 0.5s, height 0.5s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
     ball.style.transform = "translate(450%, -520%) scale(0.5)"; // Posun na ose y a zmenšení velikosti míče
+    scoreGoal()
 }
 
 function moveBallLeft() {
     if (!canMove) return; // Pokud je pohyb zakázán, nedělej nic
     canMove = false; // Zakážeme pohyb na 5 sekund
-    ball.style.transition = "transform 1s, width 1s, height 1s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
+    ball.style.transition = "transform 0.5s, width 0.5s, height 0.5s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
     ball.style.transform = "translate(-350%, -520%) scale(0.5)"; // Posun na ose y a zmenšení velikosti míče
+    scoreGoal()
 }
 
 function moveBallDown() {
     if (!canMove) return; // Pokud je pohyb zakázán, nedělej nic
     canMove = false; // Zakážeme pohyb na 5 sekund
-    ball.style.transition = "transform 1s, width 1s, height 1s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
+    ball.style.transition = "transform 0.5s, width 0.5s, height 0.5s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
     ball.style.transform = "translateY(-300%) scale(0.5)"; // Posun na ose y a zmenšení velikosti míče
+    scoreGoal()
 }
 
 function ballOpacity(){
     if (!canMove) return; // Pokud je pohyb zakázán, nedělej nic
     canMove = false; // Zakážeme pohyb na 5 sekund
-    ball.style.transition = "opacity 1s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
+    ball.style.transition = "opacity 0.5s"; // Přidáme přechodové efekty s délkou 0.5 sekundy pro transformaci, šířku a výšku
     ball.style.opacity = "0.5";
 }
 
+function scoreGoal() {
+    var goalCount = document.getElementById("scored").innerHTML;
+    goalCount = Number(goalCount) + 1;
+    document.getElementById("scored").innerHTML = goalCount;
+  }
+  
 
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("keydown", function(event) {
